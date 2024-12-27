6d47049d2ded1715aa75caf2ec902b248de52be0
akhilkumarbst014 <akhileshkumarbst0@gmail.com>
2024-12-27 15:53:07 +0530

    new-chngs_Akhilesh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import Header from '../Header/Header'
+import Carousel from '../Carousel/Carousel';
+import CurrentUpdate from '../CurrentUpdate/CurrentUpdate';
+import Info from '../Info/Info';
+import EServices from '../EServices/EServices';
 import CitizenServices from '../CitizenServices/CitizenServices';
 import GalleryAndCommunication from '../GalleryAndCommunication/GalleryAndCommunication';
 import SocialMediaSection from "../SocialMediaSection/SocialMediaSection"
+import Footer from '../Footer/Footer';
+
+
 const Home = () => {
   return (
     <>
+    <Header />
+    <Carousel />
+    <CurrentUpdate />
+    <Info />
+    <EServices />
      <CitizenServices />
      <GalleryAndCommunication />
      <SocialMediaSection/>
+     <Footer />
     </>
   )
 }
