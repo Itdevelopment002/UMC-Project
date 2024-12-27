918e25724eab20a4c3c43ef7c0c8cd154dc4ce57
unknown <itdevelopment111@gmail.com>
2024-12-27 10:25:15 +0530

    changes bybaniket

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import CitizenServices from './components/CitizenServices/CitizenServices';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
+  <>
     <div className="App">
       <CitizenServices />
+     
     </div>
+     <Footer/>
+     </>
   );
 }
 
