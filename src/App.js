b7220e3885235a76fa223f35123d5470e4ee4647
unknown <itdevelopment111@gmail.com>
2024-12-30 13:42:24 +0530

    changes by aniket

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-// import './App.css';
-// import Home from './components/Home/Home'
-
-
-// function App() {
-//   return (
-//     <>
-//       <Home />
-//     </>
-//   );
-// }
-
-// export default App;
-
+import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home/Home'
